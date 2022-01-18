5c25497601dd0199cba9c03c7c6fd25b5d68b2dc
Ajay-Kumar-2411 <ajaykumarjha2411@gmail.com>
2022-01-18 07:34:11 +0530

    Updates

---
+++ precache-manifest.60d5c4baf0bdf49045b5ccf752e48f2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f993364916d00633c74d40de7df493ce",
+    "url": "/TextFlow/index.html"
+  },
+  {
+    "revision": "7fb54f4b709c48ee4057",
+    "url": "/TextFlow/static/css/main.28f3ba4b.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/TextFlow/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "ebacbb4bd91eccb28953",
+    "url": "/TextFlow/static/js/3.bdaef63a.chunk.js"
+  },
+  {
+    "revision": "7fb54f4b709c48ee4057",
+    "url": "/TextFlow/static/js/main.2108ad2d.chunk.js"
+  },
+  {
+    "revision": "090b766ae936481b2a6e",
+    "url": "/TextFlow/static/js/runtime~main.69d9ee1d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TextFlow/static/media/logo.06e73328.svg"
+  }
+]);
